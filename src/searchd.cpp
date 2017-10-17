e55ff739c5d3acb0adff5188d582a2497f09debf
Casey Forbes <casey@nerdle.com>
2017-10-17 16:22:45 -0400

    Bump `g_iMaxPacketSize` for large sets of multi queries with filters (for faceting)
    https://www.ravelry.com/discuss/for-the-love-of-ravelry/3686589/101-125#103
    
    TODO: use the beta FACET functionality
    http://sphinxsearch.com/blog/2014/05/22/the-facet-feature/

--- src/searchd.cpp
+++ src/searchd.cpp
@@ -187,7 +187,7 @@ static int				g_iMaxCachedDocs	= 0;	// in bytes
 static int				g_iMaxCachedHits	= 0;	// in bytes
 
 static int				g_iAttrFlushPeriod	= 0;			// in seconds; 0 means "do not flush"
-int				g_iMaxPacketSize	= 8*1024*1024;	// in bytes; for both query packets from clients and response packets from agents
+int				g_iMaxPacketSize	= 16*1024*1024;	// in bytes; for both query packets from clients and response packets from agents
 static int				g_iMaxFilters		= 256;
 static int				g_iMaxFilterValues	= 4096;
 static int				g_iMaxBatchQueries	= 32;
